3fb5e01149c9db7d92de9a15d9efebcace720f6e
title <wachirapan7590@gmail.com>
2018-01-21 23:10:52 +0700

    title

---
+++ app/src/main/java/com/dev/codehouse/krixi/MainFormEdit.java
@@ -0,0 +1,13 @@
+package com.dev.codehouse.krixi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainFormEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_form_edit);
+    }
+}
